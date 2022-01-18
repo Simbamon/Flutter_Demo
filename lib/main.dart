2c377c4614da1c3f336d3fe48885c417c48b34fe
Simbamon <sbhan9511@gmail.com>
2022-01-18 18:35:26 +0900

    Stack update

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: const Text('Flutter Tutorial'),
-            ),
-            // CONTAINER
-            // body: Container(
-            //   child: const Text('This is a Flutter Tutorial'),
-            //   margin: const EdgeInsets.all(50),
-            //   padding: const EdgeInsets.all(10),
-            //   color: Colors.yellow,
-            //   height: 100,
-            //   width: 100,
-            // ),
-            body: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: const [
-                Expanded(
-                  flex: 3,
-                  child: Icon(Icons.backpack),
-                ),
-                Icon(Icons.leaderboard),
-                Icon(Icons.person),
-              ],
-            )));
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Flutter Tutorial'),
+      ),
+      // CONTAINER
+      // body: Container(
+      //   child: const Text('This is a Flutter Tutorial'),
+      //   margin: const EdgeInsets.all(50),
+      //   padding: const EdgeInsets.all(10),
+      //   color: Colors.yellow,
+      //   height: 100,
+      //   width: 100,
+      // ),
+
+      // FLEX
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: const [
+      //     Expanded(
+      //       flex: 3,
+      //       child: Icon(Icons.backpack),
+      //     ),
+      //     Icon(Icons.leaderboard),
+      //     Icon(Icons.person),
+      //   ],
+      // )
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
+          Align(
+            child: Icon(Icons.verified),
+            alignment: Alignment.center,
+          )
+        ],
+      ),
+    ));
   }
 }
