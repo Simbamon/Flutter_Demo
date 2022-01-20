f8d7b2b6780eb0ac772af4aef5b2f98957d013b3
Simbamon <sbhan9511@gmail.com>
2022-01-20 19:20:29 +0900

    List view added

--- lib/main.dart
+++ lib/main.dart
@@ -10,63 +10,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Flutter Tutorial'),
-      ),
-      // CONTAINER
-      // body: Container(
-      //   child: const Text('This is a Flutter Tutorial'),
-      //   margin: const EdgeInsets.all(50),
-      //   padding: const EdgeInsets.all(10),
-      //   color: Colors.yellow,
-      //   height: 100,
-      //   width: 100,
-      // ),
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text('Flutter Tutorial'),
+          ),
+          // CONTAINER
+          // body: Container(
+          //   child: const Text('This is a Flutter Tutorial'),
+          //   margin: const EdgeInsets.all(50),
+          //   padding: const EdgeInsets.all(10),
+          //   color: Colors.yellow,
+          //   height: 100,
+          //   width: 100,
+          // ),
 
-      // FLEX
-      // body: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //   crossAxisAlignment: CrossAxisAlignment.end,
-      //   children: const [
-      //     Expanded(
-      //       flex: 3,
-      //       child: Icon(Icons.backpack),
-      //     ),
-      //     Icon(Icons.leaderboard),
-      //     Icon(Icons.person),
-      //   ],
-      // )
+          // FLEX
+          // body: Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   crossAxisAlignment: CrossAxisAlignment.end,
+          //   children: const [
+          //     Expanded(
+          //       flex: 3,
+          //       child: Icon(Icons.backpack),
+          //     ),
+          //     Icon(Icons.leaderboard),
+          //     Icon(Icons.person),
+          //   ],
+          // )
 
-      // STACK
-      // body: Stack(
-      //   children: [
-      //     Container(
-      //       color: Colors.red,
-      //       width: 100,
-      //       height: 100,
-      //     ),
-      //     Align(
-      //       child: Icon(Icons.verified),
-      //       alignment: Alignment.center,
-      //     )
-      //   ],
-      // ),
+          // STACK
+          // body: Stack(
+          //   children: [
+          //     Container(
+          //       color: Colors.red,
+          //       width: 100,
+          //       height: 100,
+          //     ),
+          //     Align(
+          //       child: Icon(Icons.verified),
+          //       alignment: Alignment.center,
+          //     )
+          //   ],
+          // ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('pressed');
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.business), label: 'Business'),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
-        ],
-      ),
-    ));
+          // SCAFFOLD
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {
+          //     print('pressed');
+          //   },
+          // ),
+          // bottomNavigationBar: BottomNavigationBar(
+          //   items: const [
+          //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          //     BottomNavigationBarItem(
+          //         icon: Icon(Icons.business), label: 'Business'),
+          //     BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+          //   ],
+          // ),
+
+          body: ListView(
+            scrollDirection: Axis.horizontal,
+            addAutomaticKeepAlives: false,
+            children: [
+              Container(
+                color: Colors.blue,
+                width: 500,
+                height: 500,
+              ),
+              Container(
+                color: Colors.red,
+                width: 500,
+                height: 500,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 500,
+                height: 500,
+              ),
+            ],
+          )),
+    );
   }
 }
