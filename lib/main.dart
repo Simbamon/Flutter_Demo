d77f6990d00ee00b9e40c5363398ed1b6a78e583
Simbamon <sbhan9511@gmail.com>
2022-01-22 20:55:50 +0900

    Initstate and Dispose

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,18 @@ class _MyAppState extends State<MyApp> {
   int count = 0;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
